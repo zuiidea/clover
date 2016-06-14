58c8e5600046e85a449f0d4be50b6efcd4fc96fc
zuiidea <zuiidea@gmail.com>
2016-06-14 20:45:27 +0800

    home

--- src/Home.vue
+++ src/Home.vue
@@ -1,27 +1,28 @@
 <template>
-  <div>
-    <div class="logo">
-      666
-    </div>
-    <div class="slogan">
-     66666666666666666666666666
+  <div class="home">
+    <div class="box">
+      <div class="logo fade-in-bottom delay-1">
+      </div>
+      <div class="clover fade-in-bottom delay-2">
+      </div>
+      <div class="slogan fade-in-bottom delay-3">
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-const version = require('../package.json').version
-export default {
-  data () {
-    return {
-      version: version
+  const version = require('../package.json').version
+  export default {
+    data () {
+      return {
+        version: version
+      }
     }
   }
-}
 </script>
 
-<style scoped>
-  h1 {
-    color: #42b989;
-  }
+<style scoped lang="less">
+  @import './styles/normalize.css';
+  @import './styles/home.less';
 </style>
