eaae236ce7b5337883833d3601f8b1c757fefaf1
zuiidea <zuiidea@gmail.com>
2016-06-25 00:34:00 +0800

    button

--- src/components/button/index.vue
+++ src/components/button/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="weui_btn" :class="classes" :disabled="disabled">
+  <c-button class="weui_btn" :class="classes" :disabled="disabled">
     {{text}}
-  </button>
+  </c-button>
 </template>
 
 <script>
@@ -26,3 +26,20 @@ export default {
   }
 }
 </script>
+<style media="screen">
+@component button {
+  appearance: none;
+  border-radius: 4px;
+  border: 0;
+  box-sizing: border-box;
+  color: inherit;
+  display: block;
+  font-size: 18px;
+  height: 41px;
+  line-height: 2.6rem;
+  outline: 0;
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+}
+</style>
