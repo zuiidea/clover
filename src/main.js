42ea42abd16cd823277bb9861c10ca34ea9efb97
zuiidea <zuiiidea@gmail.com>
2016-10-07 23:19:31 +0800

    vue-touch

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import VueTouch from 'vue-touche'
 // import Device from './libs/device'
 import Clover from './clover'
 import FastClick from 'fastclick'
